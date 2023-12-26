2d2addad234aa1757b296e36b19373b76bd8cee6
Pratibha Awasthi <82785478+PratibhaAwasthi@users.noreply.github.com>
2023-12-25 17:17:42 -0800

    Time: 74 ms (79.62%), Space: 53.9 MB (63.90%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,24 +3,22 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-     let numHash = {};
-
-   for(let i = 0; i < nums.length; i++){
-
-       if(!numHash[nums[i]]){
-           numHash[nums[i]] = 0
-       }
-
-       numHash[nums[i]] = numHash[nums[i]] + 1
-
-
-
-       if(numHash[nums[i]]>1){
-           return true
-       }
-   }
-
-   return false
+    /*
+    brute-force
+    [1,2,3,1]
+    */
+    
+    let hashMap = {}
+    
+    for(let i = 0; i<nums.length; i++){
+        
+        if(hashMap[nums[i]] === undefined){
+            hashMap[nums[i]] = 1
+        }
+        else{
+            return true
+        }
+    }
+    
+    return false
 };
-
-
