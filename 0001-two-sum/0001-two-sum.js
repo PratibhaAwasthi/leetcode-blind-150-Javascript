cb27ec4f83f30cf0167e19fd2ec8863f12258811
Pratibha Awasthi <82785478+PratibhaAwasthi@users.noreply.github.com>
2023-12-25 18:04:08 -0800

    Time: 62 ms (60.78%), Space: 43.3 MB (25.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,31 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    /*
-    * Brute-force
-    * Space Complexity
-    * Time Complexity
 
-    for: 
-        for:
-            if(nums[i]+nums[j]=target)
-                return i,j
-
-    */
-
-    /*
-    * Hash map
-    * Space Complexity
-    * Time Complexity
-    */
-
-    let getSum = {}
-
-    for(let i = 0; i<nums.length; i++){
-        let complement = target-nums[i]
-        if(getSum[complement] !== undefined){           //check before inputing to avoid duplication
-            return [i, getSum[complement]]
+    let hashMap = {}
+    
+    for(let i = 0; i < nums.length; i++){
+        let integer2 = target - nums[i]
+        if(hashMap[nums[i]] === undefined){
+           hashMap[integer2] = i 
         }
-        getSum[nums[i]] =  i;
+        else {
+            return [ hashMap[nums[i]],i]
+        }
+    
     }
-}; 
+    
+};
