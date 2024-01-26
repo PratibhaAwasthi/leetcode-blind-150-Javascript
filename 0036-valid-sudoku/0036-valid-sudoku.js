ba40f3d78f06c00e1ad182564b91411f2f39ffaf
Pratibha Awasthi <82785478+PratibhaAwasthi@users.noreply.github.com>
2024-01-25 19:44:55 -0800

    Time: 69 ms (57.48%), Space: 53.8 MB (5.16%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -4,36 +4,45 @@
  */
 var isValidSudoku = function(board) {
     
-    /* Input: Nested Array with element as string (board)
-       Output: Boolean (true/false)
-       
-       To-do: 
-       1. Number in cells of rows are not repetative
-       2. Numbers in cells of Column not repetative
-       3. Numbers in 3x3 cell not repetative.
-       
-       r15 c15  15 
-       r55 c15  45
-       
-       set
+    /*
+    Input:Array- board
+    Output: boolean (true/false)
+    
+    To-do: 1. In row, digits between 1-9, no repeting
+    2. In column, digits between 1-9, no repeting
+    3. In sub-boxes, digits between 1-9, no repeting
+    4. Condition statement checks if its filled or not. 1-9 or .
+    
+    Note:
+    
+    Algo: 
+    sets
+    for(For)
+        if('.') continue
+        
+        if(ri_num || ci) return false
+        
+    Naming condition:
+    r1_num, c1_num, b1_5
+    r1_num, c1_3, bi/3j/3_3
+
+    
+    return true
     
     */
     
+    //Intialising set to store positiong of sudoku cells
     let set = new Set()
     
+    //Iterating over sudoku cells
     for(let i = 0; i < board.length; i++){
         for(let j =0; j < 9; j++){
-            if(board[i][j] === '.') continue
-            let num = board[i][j]
-            if(set.has(`r${[j]}_${num}`) || set.has(`c${[i]}_${num}`) || set.has(`b${parseInt(j/3)}_${parseInt(i/3)}${num}`)){
-                return false
-            }
-            else {
-             set.add(`r${j}_${num}`);
-             set.add(`c${i}_${num}`);
-             set.add(`b${parseInt(j/3)}_${parseInt(i/3)}${num}`)
-            }
+            if(board[i][j] === '.') continue                //Skipping empyt cells
             
+            if(set.has(`r${i}_${board[i][j]}`) || set.has(`c${j}_${board[i][j]}`) || set.has(`b${parseInt(i/3)}_${parseInt(j/3)}_${board[i][j]}`)) return false           //Checking duplicates 
+            set.add(`r${i}_${board[i][j]}`)                                          //Storing row position
+            set.add(`c${j}_${board[i][j]}`)                                          //Storing column position
+            set.add(`b${parseInt(i/3)}_${parseInt(j/3)}_${board[i][j]}`)            //Storing  sub-board position
         }
     }
     return true
