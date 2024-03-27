0477222b5e3de7c8a2dd7b28145d0db1b5f97ed7
Pratibha Awasthi <82785478+PratibhaAwasthi@users.noreply.github.com>
2024-03-26 18:53:31 -0700

    Time: 402 ms (20.43%), Space: 93.9 MB (47.02%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.js
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.js
@@ -1,45 +1,65 @@
-let keyTimeMap = {};
-var TimeMap = function() { 
-    keyTimeMap = {};
+/*
+
+timeKeyMap = { 
+key1: [[time1,value1], [time2,value2], ... [timeN],[valueN]],
+key2: [[time1,value1], [time2,value2], ... [timeN],[valueN]],
+.
+.
+.
+keyM: [[time1,value1], [time2,value2], ... [timeN],[valueN]]
+}
+
+*/
+
+
+let timeKeyMap = {}
+var TimeMap = function() {
+     timeKeyMap = {}
 };
 
+/** 
+ * @param {string} key 
+ * @param {string} value 
+ * @param {number} timestamp
+ * @return {void}
+ */
 TimeMap.prototype.set = function(key, value, timestamp) {
-    // If the 'key' does not exist in dictionary.
-    if (!(key in keyTimeMap)) {
-        keyTimeMap[key] = Array();
+    if (!(key in timeKeyMap)){
+        timeKeyMap[key] = []
     }
-
-    // Store '(timestamp, value)' pair in 'key' bucket.
-    keyTimeMap[key].push([ timestamp, value ]);
+    
+    timeKeyMap[key].push([timestamp, value])
 };
 
-
+/** 
+ * @param {string} key 
+ * @param {number} timestamp
+ * @return {string}
+ */
 TimeMap.prototype.get = function(key, timestamp) {
-    // If the 'key' does not exist in dictionary we will return empty string.
-    if (!(key in keyTimeMap)) {
-        return "";
-    }
     
-    if (timestamp < keyTimeMap[key][0][0]) {
-        return "";
-    }
+    if(!(key in timeKeyMap)) return ""
+    if(timeKeyMap[key][0][0] > timestamp) return ""
     
-    let left = 0;
-    let right = keyTimeMap[key].length;
-
-    while (left < right) {
-        let mid = Math.floor((left + right) / 2);
-        if (keyTimeMap[key][mid][0] <= timestamp) {
-            left = mid + 1;
-        } else {
-            right = mid;
+    let left = 0, right = timeKeyMap[key].length
+    while(left<right) {
+        let mid = Math.floor((left+right)/2)
+        
+        if (timeKeyMap[key][mid][0] <= timestamp ){
+            left = mid+1
+        } else{
+            right = mid
         }
     }
     
-    // If iterator points to first element it means, no time <= timestamp exists.
-    if (right == 0) {
-        return "";
-    }
-
-    return keyTimeMap[key][right - 1][1];
+    if(right === 0) return "";
+    
+    return timeKeyMap[key][right-1][1]
 };
+
+/** 
+ * Your TimeMap object will be instantiated and called as such:
+ * var obj = new TimeMap()
+ * obj.set(key,value,timestamp)
+ * var param_2 = obj.get(key,timestamp)
+ */
